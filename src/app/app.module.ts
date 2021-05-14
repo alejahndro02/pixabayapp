356b5779d50053211b7f388767c2971ba9687219
Alejahndro <alejahndro02@gmail.com>
2021-05-14 14:28:48 -0500

    Se separo la seccion de componentes y modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule} from '@angular/common/http'
+
+//Componentes
 import { AppComponent } from './app.component';
 import { BuscarImagenComponent } from './components/buscar-imagen/buscar-imagen.component';
 import { ListarImagenComponent } from './components/listar-imagen/listar-imagen.component';
